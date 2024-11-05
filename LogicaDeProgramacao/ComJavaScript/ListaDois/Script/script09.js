7b1df095115244b5cc10b9274e95ce662704c97f
Allan Nunes <112179619+DevNunesFerreira@users.noreply.github.com>
2024-11-04 21:47:45 -0300

    Delete LogicaDeProgramacao/ComJavaScript/ListaDois directory

--- LogicaDeProgramacao/ComJavaScript/ListaDois/Script/script09.js
+++
@@ -1,5 +0,0 @@
-// 9) Escreva um algoritmo para ler as dimensões de um quadrado (lado), calcular e escrever a área do quadrado. Dica a=ll² 
-
-var lado = parseInt(prompt("Digite o valor do lado do quadrado: "));
-var area = lado * lado;
-alert("A área do quadrado é: " + area);
